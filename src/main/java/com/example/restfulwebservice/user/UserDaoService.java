81a08a00e0faf89b85f9df44f2e4580b81bbaac0
ChoiBU <cbw0916@gmail.com>
2020-05-19 21:45:36 +0900

    Spring Boot Rest Api Get/Post

---
+++ src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -0,0 +1,39 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+    private static int userCount = 3;
+
+    static {
+        users.add(new User(1, "Kenneth", new Date()));
+        users.add(new User(2, "Alice", new Date()));
+        users.add(new User(3, "Elena", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+    public User save(User user) {
+        if(user.getId() == null) {
+            user.setId(++userCount);
+        }
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        for (User user : users) {
+            if(user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
