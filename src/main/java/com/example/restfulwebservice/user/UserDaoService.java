33c78f86427eae24ebe761c33e5e67f103d8f161
ChoiBU <cbw0916@gmail.com>
2020-05-21 21:13:21 +0900

    Add Delete element

--- src/main/java/com/example/restfulwebservice/user/UserDaoService.java
+++ src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -35,5 +36,19 @@ public class UserDaoService {
             }
         }
         return null;
-    }
+      }
+
+      public User deleteById(int id) {
+        Iterator<User> iterator = users.iterator();
+
+        while (iterator.hasNext()) {
+            User user = iterator.next();
+
+            if(user.getId() == id) {
+                iterator.remove();
+                return user;
+            }
+        }
+        return null;
+      }
 }
