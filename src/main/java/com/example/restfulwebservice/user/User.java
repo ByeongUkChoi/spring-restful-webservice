e174195774f4b71e06bfd77c164e5188d94a662b
ChoiBU <cbw0916@gmail.com>
2020-05-26 23:04:52 +0900

    Add Swagger Documentation

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -2,6 +2,8 @@ package com.example.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,17 +14,22 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
-//@JsonFilter("UserInfo")
 @NoArgsConstructor
+//@JsonFilter("UserInfo")
+@ApiModel(description = "상요자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
     @Past
+    @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요.")
     private Date joinDate;
 
+    @ApiModelProperty(notes = "사용자 패스워드를 입력해 주세요.")
     private String password;
     // 주민등록 번호
+    @ApiModelProperty(notes = "사용자 주민등록번호를 입력해 주세요.")
     private String ssn;
 }
