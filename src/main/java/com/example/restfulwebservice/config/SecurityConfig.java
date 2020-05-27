d9f39748714a4feb96dc810f1f7ce60905d56260
ChoiBU <cbw0916@gmail.com>
2020-05-27 21:53:25 +0900

    Add Spring Security and Configuarion Class

---
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.restfulwebservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("kenneth")
+                // {noop} : no operation 인코딩 없이 그대로 값을 사용한다는 뜻
+                // 실제로는 적절한 값으로 인코딩 해야함
+                .password("{noop}test1234")
+                // 로그인 완료시 부여 받는 권한
+                .roles("USER");
+    }
+}
